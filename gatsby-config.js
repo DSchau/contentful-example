d9bbd58497ecde9e383acdb13ad732376ec311f7
Dustin Schau <dustinschau@gmail.com>
2018-12-07 14:38:16 -0600

    chore: do not use host if not defined

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
       options: {
         spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
         accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
-        host: process.env.GATSBY_CONTENTFUL_HOST
+        host: process.env.GATSBY_CONTENTFUL_HOST || undefined
       }
     },
     {
